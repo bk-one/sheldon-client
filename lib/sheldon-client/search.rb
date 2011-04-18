a14630817b892fcab60b41d342b1de3e205737ae
Pontus Lindstrom <gozmo1337@gmail.com>
2011-04-18 13:46:42 +0200

    Added support to grab outgoing edges of a certain type from a node

--- lib/sheldon-client/search.rb
+++ lib/sheldon-client/search.rb
@@ -3,16 +3,37 @@ class SheldonClient
     
     protected 
     
+
     def parse_search_result( json_string )
-      JSON.parse( json_string ).map do |node_data|
-        Node.new node_data
+      JSON.parse( json_string ).map do |data|
+        if is_edge?( data )
+          Edge.new data
+        else
+          Node.new data
+        end
       end
     end
     
+    def parse_node(json_string)
+      Node.new JSON.parse( json_string )
+    end
+
+    def is_edge?( data )
+       data['from'] and data['to']
+    end
+
     def build_search_url( type, query_parameters )
       uri = Addressable::URI.parse( self.host + "/search/nodes/" + type.to_s )
       uri.query_values = query_parameters
       uri
+    end
+
+    def build_edge_search_url( node_id, type )
+      uri = Addressable::URI.parse( self.host + "/node/" + node_id.to_s + "/connections/" + type.to_s )
+    end
+
+    def build_node_url( node_id )
+      uri = Addressable::URI.parse( self.host + "/node/" + node_id.to_s)
     end
   end
 end
